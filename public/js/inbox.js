21141a63beb56079b680bdfbbce25fb64992603f
johnmickorpnt <69314516+johnmickorpnt@users.noreply.github.com>
2022-02-06 15:53:08 +0800

    Messaging UI fixes
    - Fixed Message group filter dropdown.
    - Fixed Message group filter dropdown on compose.
    - Added responsiveness for small to medium devices.
    - Added mark as important (Star icon) frontend handling.

--- public/js/inbox.js
+++ public/js/inbox.js
@@ -10,7 +10,6 @@ function makeImportant(item) {
         item.classList.add('far');
         item.classList.remove('fas');
         item.style.color = "black";
-
         // remove this message from important
     }
 }
@@ -24,7 +23,7 @@ function archiveMsg() {
 function deleteMsg() {
     confirm("Are you sure to DELETE this message?");
 }
-$(".main-item").click(function() {
+$(".main-item").click(function () {
     if (!$(this).hasClass("msg-active")) {
         $(this).addClass("msg-active");
         $(this).siblings().removeClass("msg-active");
@@ -35,51 +34,67 @@ $(".main-item").click(function() {
         $(this).removeClass("msg-active");
         $(this).find(".inbox-selector").children().prop("checked", false);
     }
-
-
 });
 
 //     I use the following piece of CSS and JavaScript. It uses an extra class dropdown-submenu. I tested it with Bootstrap 4 beta.
 
 // It supports multi level sub menus.
-$("#empty-val").click(function() {
+$("#empty-val").click(function () {
     $("#msg-group").html($(this).html());
     $("#msg-group").attr("val", "0");
     if ($("#msg-group").attr("val") == "0") {
         $("#row-to").slideUp();
     }
 });
-$(".dropdown-submenu > ul > li > a").click(function() {
+$("#msg-filter").find(".dropdown-submenu > ul > li > a").click(function () {
+    // Function for user choose to filter the messages through the groups or roles.
     $("#msg-group").html($(this).html());
-    $("#msg-group").attr("val", "1");
-
-    if ($("#msg-group").attr("val") !== "0") {
+    
+});
+$("#empty-compose-val").click(function(){
+    $("#msg-compose-group").html($(this).html());
+    $("#msg-compose-group").attr("val", "0");
+    $("#row-to").slideUp();
+});
+$("#messageModal").find(".dropdown-submenu > ul > li > a").click(function () {
+    // Function for user to choose for the recipients' group or role.
+    $("#msg-compose-group").html($(this).html());
+    $("#msg-compose-group").attr("val", "1");
+    if ($("#msg-compose-group").attr("val") !== "0") {
         $("#row-to").slideDown();
     }
 });
-$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+$('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
     if (!$(this).next().hasClass('show')) {
         $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
     }
     var $subMenu = $(this).next('.dropdown-menu');
     $subMenu.toggleClass('show');
 
-    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
         $('.dropdown-submenu .show').removeClass('show');
-        alert($(this));
     });
-
     return false;
 });
+
+$(".inbox-dropdown").on("hidden.bs.dropdown", function(){
+    $(this).find(".dropdown-submenu>ul.dropdown-menu").removeClass("show");
+});
+
+$(".star-icon").on("click",function(){
+    $(this).find(".fa-star").css("display","block");
+    $(this).find(".fa-star").removeClass("far").addClass("fas");
+    $(this).find(".fa-star").css("color","#bdb700");
+});
 $("#inbox-nav").css("height", document.getElementById('divMain').offsetHeight);
-$(document).ready(function() {
-    $('.limit-text').each(function(i, obj) {
+$(document).ready(function () {
+    $('.limit-text').each(function (i, obj) {
         if (obj.innerHTML.length >= 25) {
             obj.innerHTML = obj.innerHTML.substr(0, 20).concat("…");
         }
     });
 });
-$(document).ready(function() {
+$(document).ready(function () {
     $("#row-to").css("display", "none");
     $('#msg-body').summernote({
         height: 290,
@@ -91,7 +106,7 @@ $(document).ready(function() {
             ['para', ['ul', 'ol', 'paragraph']],
             ['table', ['table']],
             ['insert', ['link', 'picture', 'video']],
-            ['view', ['codeview', ]],
+            ['view', ['codeview',]],
         ],
     });
 });
