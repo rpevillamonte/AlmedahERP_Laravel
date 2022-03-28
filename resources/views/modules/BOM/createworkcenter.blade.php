9e082b4aa9b487596ed8b770a509e5dca7468b9d
REECE PHILIPPE VILLAMONTE <qrpevillamonte@tip.edu.ph>
2022-03-28 15:40:53 +0800

    Update createworkcenter.blade.php

--- resources/views/modules/BOM/createworkcenter.blade.php
+++ resources/views/modules/BOM/createworkcenter.blade.php
@@ -45,16 +45,16 @@
         <div class="row">
             <div class="col-6">
                 <div class="form-group">
-                    <label for="create_Work_Center_Code">Work Center Code</label>
+                    <label for="Work_Center_Code">Work Center Code</label>
 
-                    <input type="text" name="create_Work_Center_Code" id="create_Work_Center_Code" value="MOUNT-XXXX"
+                    <input type="text" name="Work_Center_Code" id="Work_Center_Code" value="MOUNT-XXXX"
                         class="form-control" disabled>
                 </div>
             </div>
             <div class="col-6">
                 <div class="form-group">
-                    <label for="create_Work_Center_label">Work Center Label</label>
-                    <input type="text" name="create_Work_Center_label" id="create_Work_Center_label" class="form-control">
+                    <label for="Work_Center_label">Work Center Label</label>
+                    <input type="text" name="Work_Center_label" id="Work_Center_label" class="form-control">
                 </div>
             </div>
         </div>
@@ -164,39 +164,39 @@
         <div class="row">
             <div class="col-6">
                 <div class="form-group">
-                    <label for="create_Production_Capacity">Production Capacity</label>
-                    <input type="number" min="1" name="create_Production_Capacity" id="create_Production_Capacity" value=""
+                    <label for="Production_Capacity">Production Capacity</label>
+                    <input type="number" min="1" name="Production_Capacity" id="Production_Capacity" value=""
                         class="form-control">
                 </div>
             </div>
             <div class="col-6">
                 <div class="form-group">
-                    <label for="create_Electricity_Cost">Electricity Cost</label>
-                    <input type="number" min="1" name="create_Electricity_Cost" id="create_Electricity_Cost" value="0" class="form-control hour_rate_compu">
+                    <label for="Electricity_Cost">Electricity Cost</label>
+                    <input type="number" min="1" name="Electricity_Cost" id="Electricity_Cost" value="0" class="form-control hour_rate_compu">
                 </div>
             </div>
             <div class="col-6">
                 <div class="form-group">
-                    <label for="create_Consumable_Cost">Consumable Cost</label>
-                    <input type="number" min="1" name="create_Consumable_Cost" id="create_Consumable_Cost" value="0" class="form-control hour_rate_compu">
+                    <label for="Consumable_Cost">Consumable Cost</label>
+                    <input type="number" min="1" name="Consumable_Cost" id="Consumable_Cost" value="0" class="form-control hour_rate_compu">
                 </div>
             </div>
             <div class="col-6">
                 <div class="form-group">
-                    <label for="create_Rent_Cost">Rent Cost</label>
-                    <input type="number" min="1" name="create_Rent_Cost" id="create_Rent_Cost" value="0" class="form-control hour_rate_compu">
+                    <label for="Rent_Cost">Rent Cost</label>
+                    <input type="number" min="1" name="Rent_Cost" id="Rent_Cost" value="0" class="form-control hour_rate_compu">
                 </div>
             </div>
             <div class="col-6">
                 <div class="form-group">
-                    <label for="create_Wages">Wages</label>
-                    <input type="number" min="1" name="create_Wages" id="create_Wages" value="0" class="form-control hour_rate_compu">
+                    <label for="Wages">Wages</label>
+                    <input type="number" min="1" name="Wages" id="Wages" value="0" class="form-control hour_rate_compu">
                 </div>
             </div>
             <div class="col-6">
                 <div class="form-group">
-                    <label for="create_Hour_rate">Hour Rate</label>
-                    <input type="number" value="0" min="0" name="create_Hour_rate" id="create_Hour_rate" value="" class="form-control" readonly>
+                    <label for="Hour_rate">Hour Rate</label>
+                    <input type="number" value="0" min="0" name="Hour_rate" id="Hour_rate" value="" class="form-control" readonly>
                 </div>
             </div>
         </div>
